d13e19ab0a8115e14c41adeee7c818a06ee09c58
Antonio <abamorim36@gmail.com>
2023-10-27 14:54:08 -0300

    Importing ImgHTMLAttributes

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,17 +1,20 @@
+import { ImgHTMLAttributes } from 'react';
 import styles from './Avatar.module.css';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
      hasBorder?: boolean;
      src?: string;
      alt?: string;
+     title?: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, src, alt, title }: AvatarProps) {
      return (
           <img 
                className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
                src={src}
-               alt={alt} 
+               alt={alt}
+               title={title}
           />
      );
 }
